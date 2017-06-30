269453852b142e18ab06efc90559a5804c920f6a
Viktor Ivashchenkov <mrcrabs67forms@outlook.com>
2017-06-30 14:27:37 +0300

    Первый вариант списка результатов issue #2

---
+++ src/components/Results.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="results">
+        <div class="container">
+            <table class="table">
+  <thead>
+    <tr>
+      <th>page_title</th>
+      <th>url</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="result in results">
+      <td>{{result.page_title}}</td>
+      <td>{{result.url}}</td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      results: [
+        {
+          'id': 1,
+          'page_title': 'Bootstrap Vue',
+          'url': 'bootstrap-vue.js.org'
+        },
+        {
+          'id': 2,
+          'page_title': 'C++ — Википедия',
+          'url': 'ru.wikipedia.org/wiki/C%2B%2B'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
