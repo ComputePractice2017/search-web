1a5cf155a356c1bbfb28bbc6f595bcac6ba14df7
Viktor Ivashchenkov <mrcrabs67forms@outlook.com>
2017-07-01 17:06:09 +0300

    Страница поиска. issue #2

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="results">
+    <div class="logo">
+    <img src="https://pp.userapi.com/c841328/v841328106/4785/AGH_PNmeyes.jpg" alt="1" width="60%" height="90%">
+    </div>
     <div class="container">
       <form>
         <div class="form-group">
-          <label for="exampleInputEmail1">Поисковая страница</label>
-          <br>
-          <input v-model="message" placeholder="Введите запрос" size="100">
+          <input v-model="message" placeholder="Введите запрос" size="70">
           <button type="button" class="btn btn-primary btn-sm">Поиск</button>
+          <br><br><br><br><br><br>
+          <b>ИАТЭ НИЯУ МИФИ</b>
+          <br>
+          <b>Практика 2017</b>
         </div>
       </form>
     </div>
@@ -19,10 +24,22 @@
 
 <style>
 input{
-
   border: 2px groove #B9B9BC;
 }
 button{
   height: 30px;
+}
+.results{
+  position: relative;
+}
+label{
+  font-size: large;
+}
+.logo{
+  padding: 2%;
+  text-align: center;
+}
+.container{
+  text-align: center;
 }
 </style>
